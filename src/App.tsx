70f0a3f60c90c68cb61baf35c71e69e4d2005644
NewJourney-LiNing <577526282@qq.com>
2021-11-03 14:33:24 +0800

    新增分页组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { defineComponent } from 'vue';
 import './style/main.scss'
 import Icon from './example/Icon/index'
+import AtPagination from './example/AtPagination/index'
+
 export default defineComponent({
   name: 'App',
   setup() {
     return () => (
       <>
         <Icon></Icon>
+        <AtPagination />
       </>
     );
   }
--- src/example/Icon/index.tsx
+++ src/example/Icon/index.tsx
@@ -26,6 +26,7 @@ export default defineComponent({
                         </svg>
                     </SIcon>
                 </div>
+                
             </div>
         )
     }
